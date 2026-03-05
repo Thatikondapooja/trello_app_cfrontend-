ce4336d30a12a28f2076f7adf78ad58ce682844e
Thatikondapooja <thatikondapooja888@gmail.com>
2026-03-05 14:17:49 +0530

    remove Remove withCredentials: true.

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL, // NestJS backend
-    withCredentials: true,
+ 
 });
 console.log("API BASE URL:", process.env.REACT_APP_API_URL);
 
